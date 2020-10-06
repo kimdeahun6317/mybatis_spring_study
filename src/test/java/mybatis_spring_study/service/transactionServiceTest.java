274d3c6d49cb3a49c0b7ea22fa333aa044fa25f1
kimdeahun6317 <kdh0161@naver.com>
2020-10-06 15:14:58 +0900

    ^^

--- src/test/java/mybatis_spring_study/service/transactionServiceTest.java
+++ src/test/java/mybatis_spring_study/service/transactionServiceTest.java
@@ -1,7 +1,5 @@
 package mybatis_spring_study.service;
 
-import static org.junit.Assert.fail;
-
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
 import org.junit.After;
@@ -36,7 +34,7 @@ public class transactionServiceTest {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
 		Department department = new Department(1, "태스크포스", 10); // DuplicateKeyException
 		Employee employee = new Employee(1004, "박신혜", "과장", new Employee(4377), 4100000, department);
-		service.registerTransaction(department, employee);
+		service.trRegister(department, employee);
 	}
 
 	@Test(expected = DuplicateKeyException.class)
@@ -44,7 +42,7 @@ public class transactionServiceTest {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
 		Department department = new Department(6, "태스크포스", 10); // DuplicateKeyException
 		Employee employee = new Employee(4377, "박신혜", "과장", new Employee(4377), 4100000, department);
-		service.registerTransaction(department, employee);
+		service.trRegister(department, employee);
 	}
 
 	@Test
@@ -52,7 +50,7 @@ public class transactionServiceTest {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
 		Department department = new Department(6, "태스크포스", 10); // DuplicateKeyException
 		Employee employee = new Employee(1005, "박신혜", "과장", new Employee(4377), 4100000, department);
-		service.registerTransaction(department, employee);
+		service.trRegister(department, employee);
 	}
 
 	@Test(expected = RuntimeException.class)
@@ -60,7 +58,7 @@ public class transactionServiceTest {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
 		Department department = new Department(100); // RuntimeException -> rollback
 		Employee employee = new Employee(1005); // rollback 되므로삭제되면안됨
-		service.unRegisterTransaction(department, employee);
+		service.trUnRegister(department, employee);
 	}
 
 	@Test(expected = RuntimeException.class)
@@ -68,7 +66,7 @@ public class transactionServiceTest {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
 		Department department = new Department(100); // RuntimeException -> rollback
 		Employee employee = new Employee(9999); // rollback 되므로삭제되면안됨
-		service.unRegisterTransaction(department, employee);
+		service.trUnRegister(department, employee);
 	}
 
 	@Test
@@ -76,7 +74,7 @@ public class transactionServiceTest {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
 		Department department = new Department(6); // RuntimeException -> rollback
 		Employee employee = new Employee(1005); // rollback 되므로삭제되면안됨
-		service.unRegisterTransaction(department, employee);
+		service.trUnRegister(department, employee);
 	}
 
 }
